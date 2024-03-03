5429eb54630f7c9beea7cf5100034230b4ec5856
Razdor13 <oneamaday@gmail.com>
2024-03-03 21:07:12 +0200

    worked with validation

--- src/components/worker-add-form/worker-add-form.js
+++ src/components/worker-add-form/worker-add-form.js
@@ -1,15 +1,23 @@
 import { Component } from 'react'
 import './worker-add-form.css'
 
+
+
+
 class WorkerAddForm extends Component{
     constructor(props){
         super(props)
         this.state = {
             name : '',
-            salary : ''
+            salary : '',
+            isValidName : true,
+            isValidSalary : true,
         }
         
+        
     }
+    
+    
 
     onValueChange = (e) => {
         this.setState({
@@ -21,14 +29,45 @@ class WorkerAddForm extends Component{
         const {onCreate} =this.props
         e.preventDefault();
         onCreate(this.state)
+        if (this.state.name.trim()) {
+            this.setState({
+                isValidName : false
+            })
+        }else{
+            this.setState({
+                isValidName : true
+            })
+        }
+        if (this.state.salary.trim()) {
+            this.setState({
+                isValidSalary : false
+            })
+        }else{
+            this.setState({
+                isValidSalary : true
+            })
+        }
+        console.log(this.state.isValidName)
+
     }
     render() {
+        const validFormStyles = {
+            border: '2px solid transparent',
+            boxShadow: 'none' 
+        };
+        const invalidFormStyles = {
+            border: '2px solid red',
+            boxShadow: '0 0 5px red',
+        };
+        const isValidName = this.state.isValidName ?   invalidFormStyles : validFormStyles
+        const isValidSalary = this.state.isValidSalary ? invalidFormStyles : validFormStyles 
         const {name,salary} = this.state
         return (<div className="app-add-form">
             <h3>Додати нового співробітника</h3>
             <form
                 className="add-form d-flex">
                 <input type="text"
+                    style={isValidName}
                     className="form-control new-post-label"
                     placeholder="Як його звуть?" 
                     value={name}
@@ -36,6 +75,7 @@ class WorkerAddForm extends Component{
                     name='name'
                     />
                 <input type="number"
+                    style={isValidSalary}
                     className="form-control new-post-label"
                     placeholder="З/П в $?" 
                     value={salary}
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App></App>
+  <StrictMode><App></App></StrictMode>
 );
 
